5609a2f3ddcae2b6f960b811e563b796029967da
Patricio Mena <luispatricio.mh@gmail.com>
2023-10-12 17:40:06 -0600

    updated cta, added contact page and sucess page

--- build/js/main.js
+++ build/js/main.js
@@ -94,7 +94,7 @@ let gallery = document.getElementById('screenshots-gallery');
 let moveVal = 0;
 
 moveGallery = () => {
-  moveVal = window.scrollY * 0.3;
+  moveVal = window.scrollY * 0.4;
   gallery.style.transform = `translate3d(-${moveVal}px, 0, 0)`;
   requestAnimationFrame(moveGallery);
 };
@@ -119,56 +119,7 @@ const hiddenSection = document.querySelectorAll('.anim');
 // Telling the observer what to observe
 hiddenSection.forEach((el) => scrollObserver.observe(el));
 
-///////////////////////////////////////////////
-// Dark Mode Switch
-
-// Icons
-const sunIcon = document.getElementById('theme-toggle-dark-icon');
-const moonIcon = document.getElementById('theme-toggle-light-icon');
-const themeToggle = document.getElementById('theme-toggle');
-
-// Theme Vars
-const userTheme = localStorage.getItem('theme');
-const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-// Icon Toggling
-const iconToggle = () => {
-  moonIcon.classList.toggle('hidden');
-  sunIcon.classList.toggle('hidden');
-};
-
-// Initial Theme Check
-const themeCheck = () => {
-  if (userTheme === 'dark' || (!userTheme && systemTheme)) {
-    document.documentElement.classList.add('dark');
-    moonIcon.classList.add('hidden');
-    return;
-  }
-  sunIcon.classList.add('hidden');
-};
-
-// Manual Theme Switch
-const themeSwitch = () => {
-  if (document.documentElement.classList.contains('dark')) {
-    document.documentElement.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
-    iconToggle();
-    return;
-  }
-  document.documentElement.classList.add('dark');
-  localStorage.setItem('theme', 'dark');
-  iconToggle();
-};
-
-// call theme switch on clickin buttons
-// sunIcon.addEventListener('click', themeSwitch);
-// moonIcon.addEventListener('click', themeSwitch);
-themeToggle.addEventListener('click', themeSwitch);
-
-// invoke theme check on initial load;
-themeCheck();
-
-// set current year
+/////////// set current year
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
